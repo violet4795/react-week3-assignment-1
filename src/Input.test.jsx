501dc51f6a8746acc0160657fae15058170c7dc9
violet <violet4795@naver.com>
2022-08-18 00:02:08 +0900

    아직 뭐가뭔지 모르겠는데./..

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,9 +1,28 @@
+import { render, fireEvent } from '@testing-library/react';
 import Input from './Input';
 
 test('Input', () => {
-  const onChange = jest.fn();
-  const onClick = jest.fn();
-  const x = Input({ value: '할 일', onChange, onClick });
+  const value = '나는 바보다';
+  const handleClick = jest.fn(() => value);
+  function handleChange(e) {
+    return e.target.value;
+  }
+  //   const handleChange = jest.fn((e) => console.log(e));
+  //   handleChange.mockReturnValue(value);
 
-  expect(x).
+  //   console.log(handleChange());
+  const { container, getByText, getByRole } = render((
+    <Input
+      value={value}
+      onChange={handleChange}
+      onClick={handleChange}
+    />
+  ));
+
+  expect(container).toHaveTextContent('할 일추가');
+  expect(handleChange).not.toBeCalled();
+
+  fireEvent.keyDown(getByRole('textbox'), { key: 'A' });
+  expect(handleChange).toBeCalledWith('A');
+//   fireEvent.click(getByText('추가'));
 });
